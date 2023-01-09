4fb648eadd73c12140c0c9104ba86b76407ce844
Mani Heshmati <Maniheshmati1385@gmail.com>
2023-01-09 07:28:24 +0330

    Update solarSystem.py

--- solarSystem.py
+++ solarSystem.py
@@ -1,13 +1,22 @@
 import requests
 
-userInput = input("Enter yout Planer, moon etc... name: ")
-apiLink = f'https://api.le-systeme-solaire.net/rest.php/bodies/{userInput}' 
 
-r = requests.get(apiLink)
+def appOps():
+    userInput = input("Enter yout Planer, moon etc... name: ")
+    apiLink = f'https://api.le-systeme-solaire.net/rest.php/bodies/{userInput}' 
+    r = requests.get(apiLink)
+    
+    if r.status_code == 200:
+        latinName = r.json()['name']
+        name = r.json()['englishName']
+        isPlanet = r.json()['isPlanet']
+        moons = r.json()['moons']
+        typeOfObject = r.json()['bodyType']
+        print(f"Latin Name: {latinName}\nEnglish Name: {name}\nis Planet: {isPlanet}\nmoons: {moons}\nType Of Object: {typeOfObject}")
 
-latinName = r.json()['name']
-name = r.json()['englishName']
-isPlanet = r.json()['isPlanet']
-moons = r.json()['moons']
-typeOfObject = r.json()['bodyType']
-print(f"Latin Name: {latinName}\nEnglish Name: {name}\nis Planet: {isPlanet}\nmoons: {moons}\nType Of Object: {typeOfObject}")
+    else:
+        print(r.status_code)
+        print("\n There was an error. try again later... \n")
+        appOps()
+
+appOps()
