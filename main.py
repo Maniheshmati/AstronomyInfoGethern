c74e1b67d2493f5d80dd1f22aa9dbf9343388265
Mani <maniheshmati1385@gmail.com>
2023-01-11 08:27:09 +0330

    python script added.

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 import requests
 
-def myRequest(url):
-    pass
 
+def appOps():
+    moon = []
+    userInput = input("Enter yout Planet, moon,dwarf planet etc... name: ")
+    apiLink = f'https://api.le-systeme-solaire.net/rest.php/bodies/{userInput}' 
+    r = requests.get(apiLink)
+    
+    if r.status_code == 200:
+        latinName = r.json()['name']
+        name = r.json()['englishName']
+        isPlanet = r.json()['isPlanet']
+        moons = r.json()['moons']
+        for i in moons:
+            moon.append(i['moon'])
+        typeOfObject = r.json()['bodyType']
+        print(f"Latin Name: {latinName}\nEnglish Name: {name}\nis Planet: {isPlanet}\nmoons: {moon}\nType Of Object: {typeOfObject}")
+
+    else:
+        print(r.status_code)
+        print("\n There was an error. try again later... \n")
+        appOps()
+
+appOps()
