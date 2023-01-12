fefa2a725895d39373c1793a03ab7be19af087c6
Mani <maniheshmati1385@gmail.com>
2023-01-12 11:12:24 +0330

    "Maked a list to user select his/her option and see the answer without need to typing"

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import requests
+import inquirer
 
 
-def appOps():
+
+
+
+def mainApp():
+    global answer
+    questions = [ inquirer.List('userSelection',
+                message="Select your choice:",
+                choices=['space natural things', 'space shuttles'],
+            ),
+]
+    answer = inquirer.prompt(questions)
+    
+    if answer['userSelection'] == 'space natural things':
+        spaceNaturalThings()
+
+    
+def spaceNaturalThings():
     moon = []
     userInput = input("Enter yout Planet, moon,dwarf planet etc... name: ")
     apiLink = f'https://api.le-systeme-solaire.net/rest.php/bodies/{userInput}' 
@@ -13,21 +30,31 @@ def appOps():
         name = r.json()['englishName']
         isPlanet = r.json()['isPlanet']
         moons = r.json()['moons']
-        for i in moons:
-            moon.append(i['moon'])
+        if moons != None:
+             for i in moons:
+                moon.append(i['moon'])
+                
         typeOfObject = r.json()['bodyType']
-        # print(f"Latin Name: {latinName}\nEnglish Name: {name}\nis Planet: {isPlanet}\nmoons: {moon}\nType Of Object: {typeOfObject}")
+
+        questions = [ inquirer.List('userSelectionPlanet',
+            message="Select your choice:",
+            choices=['name', 'moons', 'object type'],
+            ),
+        ]
+        userAnswer = inquirer.prompt(questions)
+        
+        if userAnswer['userSelectionPlanet'] == 'name':
+            print(name)
+        elif userAnswer['userSelectionPlanet'] == 'moons':
+            print(moon)
+        elif userAnswer['userSelectionPlanet'] == 'object type':
+            print(typeOfObject)
+        
 
     else:
-        # print(r.status_code)
-        # print("\n There was an error. try again later... \n")
-        # appOps()
-        pass
+        print(r.status_code)
+        print("\n There was an error. try again later... \n")
+        spaceNaturalThings()
 
-
-def mainApp():
-    appOps()
-    print(latinName)
 
 mainApp()
-print("Done")
